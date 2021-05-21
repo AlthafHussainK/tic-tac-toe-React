014a9ae5fe6efec226af0c5e270eebafc2b1dca7
AlthafHussainK <althafakm@gmail.com>
2021-05-21 07:20:29 +0530

    fix a typo

--- src/helpers.js
+++ src/helpers.js
@@ -9,7 +9,7 @@ export function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6]
   ];
-  for (let i=0; i < lines.lengthl i++) {
+  for (let i=0; i < lines.length; i++) {
     const [a, b, c] = lines[i]
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a]
