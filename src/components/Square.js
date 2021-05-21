c3cc884b19f78ee7908d7aa58b240d6b9d4ec455
AlthafHussainK <althafakm@gmail.com>
2021-05-21 07:12:12 +0530

    Sqaure styling

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-
+const style = {
+  background: 'lightblue',
+  padding: '10px 15px',
+  border: '2px solid darkblue',
+  fontSize: '30px',
+  fontWeight: '800',
+  cursor: 'pointer',
+  outline: 'none'
+}
 
 const Square = ({ value, onClick }) => (
-  <button onClick={onClick}>
+  <button style={style} onClick={onClick}>
     {value}
   </button>
 )
