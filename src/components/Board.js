3859eefdf26db4179dd9394e243eb9b7100defee
AlthafHussainK <althafakm@gmail.com>
2021-05-21 07:17:29 +0530

    Board styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,28 @@
 import React from 'react'
+import Sqaure from './Square'
 
-const Board = () => <div>Board</div>
+const style = {
+  border: '4px solid darkblue',
+  borderRadius: '10px',
+  width: '250px',
+  height: '250px',
+  margin: '0 auto',
+  display: 'grid',
+  gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+}
+
+const Board = ({ squares, onClick }) => (
+  <div style={style}>
+    <Sqaure value="1" onClick={() => onClick("dummy")} />
+    <Sqaure value="2" onClick={() => onClick("dummy")} />
+    <Sqaure value="3" onClick={() => onClick("dummy")} />
+    <Sqaure value="4" onClick={() => onClick("dummy")} />
+    <Sqaure value="5" onClick={() => onClick("dummy")} />
+    <Sqaure value="6" onClick={() => onClick("dummy")} />
+    <Sqaure value="7" onClick={() => onClick("dummy")} />
+    <Sqaure value="8" onClick={() => onClick("dummy")} />
+    <Sqaure value="9" onClick={() => onClick("dummy")} />
+  </div>
+)
 
 export default Board
