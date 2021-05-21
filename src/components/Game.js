8643ab7fdcdc1e1eff40cd34d10447e19292eb8a
AlthafHussainK <althafakm@gmail.com>
2021-05-21 07:29:10 +0530

    create state

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,22 @@
-import React, { useImperativeHandle } from 'react'
+import React, { useState } from 'react'
+import { calculateWinner } from '../helpers'
 import Board from './Board'
 
 const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [xIsNext, setXisNext] = useState(true)
+  const winner = calculateWinner(board)
 
   const handleClick = () => {
 
   }
 
   const jumpTo = () => {
-    
+
   }
 
   return (
-    <Board onClick={handleClick} />
+    <Board squares={board} onClick={handleClick} />
   )
 }
 
